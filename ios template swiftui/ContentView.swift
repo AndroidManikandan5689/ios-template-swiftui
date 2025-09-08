3eb8a96d134c7c63f76b37b8d650f7f3e52c257d
Manikandan Kasinathan <manikandankasi@fss.co.in>
2025-09-08 15:52:08 +0530

    Initial Commit

---
+++ ios template swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios template swiftui
+//
+//  Created by AndroidMani on 08/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios template swiftui/ios_template_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_template_swiftuiApp.swift
+//  ios template swiftui
+//
+//  Created by AndroidMani on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_template_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
